2207c1131170c5ac39df9dcd54c9a49b229dcc6d
levis <levisims1014@yahoo.com>
2019-02-13 09:01:56 -0500

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+public class Class1
+{
+    public class UserInfo
+        {
+        [Required]
+        public string FirstName { set; get; }
+
+        [Required]
+        public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Bad Email!")]
+        public string Email { set; get; }
+
+        [Required]
+        public string Phone { set; get; }
+
+
+        [Required]
+        public string Password { set; get; }
+
+        public UserInfo()
+        {
+
+        }
+
+        public UserInfo(string firstname, string lastname, string email, string phone, string password)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+    }
+    
+}
---
+++ Lab 21/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            string[] UserDetails = { "First Name", "Last Name", "Email", "Phone Number", "Password" };
+            ViewBag.UserDetails = User;
+            return View();
+        }
+        public ActionResult Result(User u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            return View();
+        }
+
+    }
+}
